154b1dd1bba95c5bdcac6b2a411dcd0daa009d6f
mihaelcruz <mihael.cruz@gmail.com>
2019-08-18 14:41:08 -0400

    Update soap_cliente.php

--- soap_cliente.php
+++ soap_cliente.php
@@ -7,7 +7,9 @@ $soap_cliente = new SoapClient('http://uasb-api.rootcode.com.bo/mcruz/api-soap/s
 
 $resultado_suma = $soap_cliente->sumar(7, 5);
 $resultado_resta = $soap_cliente->restar(8, 3);
-echo $resultado_suma." --- ".$resultado_resta;
+echo "SOAP PHP PARA LA SUMA Y RESTA DE DOS NUMEROS <br>"	
+echo "La suma de 7 mas 5 es:".$resultado_suma."<br>";
+echo "La resta de 8 menos 3 es:".$resultado_resta;
 } catch (Exception $e) {
     echo 'Error --> '. $e->getMessage();
 }
