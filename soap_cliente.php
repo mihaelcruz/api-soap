8fcb6f7acbfb2cba34421ed44bd404e1076174bb
mihaelcruz <mihael.cruz@gmail.com>
2019-08-18 14:21:24 -0400

    Update soap_cliente.php

--- soap_cliente.php
+++ soap_cliente.php
@@ -3,7 +3,7 @@ try{
 	$options = array(
   'cache_wsdl' => 0,
 );
-$soap_cliente = new SoapClient('mihaelcruz.com/api-soap/soap_wsdl.wsdl', $options);
+$soap_cliente = new SoapClient('http://uasb-api.rootcode.com.bo/mcruz/api-soap/soap_wsdl.wsdl', $options);
 
 $resultado_suma = $soap_cliente->sumar(7, 5);
 $resultado_resta = $soap_cliente->restar(8, 3);
